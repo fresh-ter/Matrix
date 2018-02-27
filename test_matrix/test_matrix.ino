c106ba60be65c3ddf4b305c8a923b19f398e86c7
Terminator <alexey.orlov2016@yandex.ru>
2018-02-27 21:32:57 +0400

    Add files via upload

---
+++ test_matrix/test_matrix.ino
@@ -0,0 +1,14 @@
+#include <Matrix.h>
+
+int pinDigits[4] = {2,12,11,8};
+int pinPins[8] = {9,13,4,6,7,10,3,5};
+Matrix m(pinDigits,pinPins);
+
+void setup()
+{
+
+}
+void loop()
+{
+    m.showMatrix(1024,0);
+}
